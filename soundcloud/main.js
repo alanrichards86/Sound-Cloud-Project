ba073e6b1b9eff7e59a1f31470ab2ada6cd2eba0
Alan Richards <alanrichards86@gmail.compwdlsexitstopopengitopengit config --global user.name Alan>
2017-06-16 14:52:50 -0400

    Trying to get fetch to work currently in JS

--- soundcloud/main.js
+++ soundcloud/main.js
@@ -4,12 +4,41 @@
 
 // 1. First select and store the elements you'll be working with
 
+var audio = document.querySelector('#audioPlayer');
+var form = document.querySelector('#mainForm');
+var request = new Request('https://api.soundcloud.com/tracks?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f')
+var myInit = { method: 'GET',
+               mode: 'no-cors',
+               cache: 'default' };
 
 // 2. Create your `onSubmit` event for getting the user's search term
+function sub() {
+  console.log('Hi!');
+  alert('It worked!');
+}
 
+
+// function sub() {
+//   console.log('Hi!');
+//   fetch('https://api.soundcloud.com/users?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f', myInit).then(
+//     function(response) {
+//      console.log(response);
+//      console.log('Status:', response.status);
+//     response.json().then(function(data){
+//       console.log(data);
+//     });
+//
+//    });
+//  }
+//     function(reject){
+//       console.log("Rejected!");
+//     }
+//   );
+//   // alert(form.artist.value);
+//
+// }
 
 // 3. Create your `fetch` request that is called after a submission
-
 
 // 4. Create a way to append the fetch results to your page
 
